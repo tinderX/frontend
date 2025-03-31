31951ac329a6384a4340de7b02b68328cea286d9
Merzki <guforski@gmail.com>
2025-03-31 08:43:41 +0200

    add beta-version footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule], 
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  lastDelta = 0;
+  showFooter = false;
+
+  @HostListener('window:wheel', ['$event'])
+  onWheel(event: WheelEvent): void {
+    const delta = event.deltaY;
+    
+    if (delta > 0) {
+      this.showFooter = true; 
+    } else if (delta < 0) {
+      this.showFooter = false; 
+    }
+  }
+}
